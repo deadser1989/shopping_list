3f95f2b35f80d553417148e856c60a261fcdebf6
Artem Lazarenko <deadser1989@gmail.com>
2025-03-27 00:42:58 +0300

    Fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/shopping_list/' 
-  },
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  output: { publicPath: './' }  
 })
