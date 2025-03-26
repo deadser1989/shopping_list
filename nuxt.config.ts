f19f58c69dc983e634b5c50fab5da70abab3a055
Artem Lazarenko <deadser1989@gmail.com>
2025-03-27 00:33:50 +0300

    Fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/shopping_list/' 
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
